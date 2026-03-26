faa28051afb0a4cbcb24d12e1b6454a2e1c77723
JohnzzzAntony <johnsjoantony@gmail.com>
2026-03-26 11:10:02 +0400

    Cleanup temporary script

--- reset_pass.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-try:
-    u = User.objects.get(username='admin')
-    u.set_password('Admin123$$')
-    u.save()
-    print("SUCCESS")
-except User.DoesNotExist:
-    User.objects.create_superuser('admin', 'admin@example.com', 'Admin123$$')
-    print("CREATED")
